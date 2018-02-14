1e01e43b53f1dcdd889a6a9aa1f3717fc9e2c660
Brian Bixby <brianbixby0@gmail.com>
2018-02-14 12:52:28 -0800

    challenge 3 done

---
+++ challenge-03/solution.js
@@ -0,0 +1,22 @@
+let testEngine = {
+  value: 2,
+  next: {
+    value: 10,
+    next: {
+      value: 30
+    }
+  }
+}
+
+const traverse = (engine) => {
+  let passengers = [engine.value];
+  let currentLocation = 'engine.next';
+  while(eval(currentLocation)) {
+    passengers.push(+eval(currentLocation).value);
+    currentLocation += '.next';
+  }
+  return passengers.reduce((acc, cur) =>  acc + cur);
+}
+
+// returns 42
+traverse(testEngine);
