933f538eda317ba9ffe68a8156ed235e135187f5
Brian Bixby <brianbixby0@gmail.com>
2018-04-10 01:24:56 -0700

    refactored bst remove

--- challenge-23/solution.js
+++ challenge-23/solution.js
@@ -38,32 +38,52 @@ exports.BST = class {
       }
     }
   }
-
-  treeNode(value, left=null, right=null) {
-    return {
-      value: value,
-      left: left,
-      right: right,
-    };
-  }
-
-  removeNode(val){
-    let arr = [];
-  
-    _traverse(this.root);
-    function _traverse(node) {
-      if(node.value !== val) {
-        arr.push(node.value);
+  removeNode(val) {
+    let currentNode, parentNode, nextBiggestParentNode=null, found=false, base=[this.root];
+    while(base.length > 0 && !found) {
+      currentNode = base.pop();
+      if(currentNode.value === val) {
+        found=true;
+        if(!currentNode.left && !currentNode.right) {
+          parentNode.right === currentNode ? parentNode.right = null : parentNode.left = null;
+        }
+        else if(!currentNode.right && currentNode.left) {
+          parentNode.right === currentNode ? parentNode.right = currentNode.left : parentNode.left = currentNode.left;
+        }
+        else if(!currentNode.left && currentNode.right) {
+          parentNode.right === currentNode ? parentNode.right = currentNode.right : parentNode.left = currentNode.right;
+        }
+        else {
+          let _traverse = node => {
+            if (node.right) {
+              nextBiggestParentNode = node;
+              _traverse(node.right);
+            }
+            else {
+              currentNode.value = node.value;
+              nextBiggestParentNode ? nextBiggestParentNode.right = null : currentNode.left = null;
+            }
+          }
+          _traverse(currentNode.left);
+        }
       }
-      if (node.left) _traverse(node.left);
-      if (node.right) _traverse(node.right);
+      else {
+        parentNode = currentNode;
+        val > currentNode.value && currentNode.right ? base.unshift(currentNode.right) : base.unshift(currentNode.left);
+      }
     }
-  
-    let newbst = new this.constructor();
-    for (let i of arr) {
-      newbst.insert(this.treeNode(i));
-    }
-    console.log(JSON.stringify(newbst, null, 2));
-    return newbst;
-  };
-};
+    return this;
+  }
+}
+
+// let bst = new BST();
+// const nums = [20,10,5,15,3,7,13,17,30,35,25,23,27,37,36,38];
+// function createBst() {
+//   for (let i of nums) {
+//     bst.insert(new TreeNode(i));
+//   }
+//   console.log(JSON.stringify(bst, null, 2));
+//   bst.removeNode(35);
+// }
+// createBst();
+// console.log(JSON.stringify(bst, null, 2));
